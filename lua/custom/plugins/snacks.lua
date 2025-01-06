28bdf4b56fc800de037e9002541a63af40352f96
David Basile Filho <davidbasilefilho@gmail.com>
2025-01-06 18:56:29 -0300

    Improved window navigation and terminal experience

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -1,5 +1,24 @@
 return {
   'folke/snacks.nvim',
+  dependencies = {
+    'folke/edgy.nvim',
+    opts = function(_, opts)
+      for _, pos in ipairs { 'top', 'bottom', 'left', 'right' } do
+        opts[pos] = opts[pos] or {}
+        table.insert(opts[pos], {
+          ft = 'snacks_terminal',
+          size = { height = 0.4 },
+          title = '%{b:snacks_terminal.id}: %{b:term_title}',
+          filter = function(_buf, win)
+            return vim.w[win].snacks_win
+              and vim.w[win].snacks_win.position == pos
+              and vim.w[win].snacks_win.relative == 'editor'
+              and not vim.w[win].trouble_preview
+          end,
+        })
+      end
+    end,
+  },
   priority = 1000,
   lazy = false,
   config = function()
@@ -23,21 +42,22 @@ return {
     }
 
     -- Keymaps
+    vim.keymap.set('t', '<C-q>', [[<C-\><C-n>]], { desc = 'Exit terminal mode' })
 
     map({ 'n', 'v' }, '<leader>l', function()
-      Snacks.lazygit()
+      snacks.lazygit()
     end, { desc = '[L]azygit' })
 
     map({ 'n', 'v' }, '<leader>tt', function()
-      snacks.terminal.open(nil)
+      snacks.terminal(nil, { win = { height = 0.2, relative = 'editor' } })
     end, { desc = '[T]oggle [T]erminal' })
 
     map({ 'n', 'v' }, '<leader>tr', function()
-      snacks.terminal.open(nil, { win = { position = 'right' } })
+      snacks.terminal.open({ 'bash' }, { win = { position = 'right', relative = 'editor' } })
     end, { desc = '[T]oggle [R]ight terminal' })
 
     map({ 'n', 'v' }, '<leader>tf', function()
-      snacks.terminal.open 'bash'
+      snacks.terminal 'bash'
     end, { desc = '[T]oggle [F]loating terminal' })
   end,
 }
