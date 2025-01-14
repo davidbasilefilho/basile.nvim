e00b908ea4b57fb8ae313b6b8f43da36f0aa6702
David Basile Filho <davidbasilefilho@gmail.com>
2025-01-14 15:41:36 -0300

    scrapped the idea of making it a 'distro'

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -15,8 +15,6 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-vim.opt.rtp:append(vim.fn.stdpath("config"):gsub("/nvim$", "/nvconf"))
-
 -- Setup lazy.nvim
 require("lazy").setup({
 	spec = {
