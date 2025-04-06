adfa616df03f713882a7ce35fa9ecc96ef306062
David Basile Filho <davidbasilefilho@gmail.com>
2025-04-06 12:56:05 -0300

    better themes

---
+++ lua/plugins/themes.lua
@@ -0,0 +1,17 @@
+return {
+	{
+		"tiagovla/tokyodark.nvim",
+	},
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+		end
+	},
+	{
+		"rebelot/kanagawa.nvim",
+		config = function()
+		end
+	}
+}
