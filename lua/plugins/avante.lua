5be605a56db60d85aa3bc9df64183b9d7d5103cf
David Basile Filho <davidbasilefilho@gmail.com>
2025-05-10 15:48:00 -0300

    added mcphub

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -92,6 +92,17 @@ return {
 				enable_cursor_planning_mode = true,
 				minimize_diffs = true,
 			},
+
+			system_prompt = function()
+				local hub = require("mcphub").get_hub_instance()
+				return hub:get_active_servers_prompt()
+			end,
+			-- The custom_tools type supports both a list and a function that returns a list. Using a function here prevents requiring mcphub before it's loaded
+			custom_tools = function()
+				return {
+					require("mcphub.extensions.avante").mcp_tool(),
+				}
+			end,
 		},
 		-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
 		build = "make",
